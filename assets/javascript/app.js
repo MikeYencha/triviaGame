9675fd333c30b8a1a2660872d1d4398627d635fa
MikeYencha <jmyeniii@icloud.com>
2017-09-13 09:47:51 -0400

    it's almost finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,104 +1,79 @@
-// var questions = ['what pigment makes red roses red?', 'Which is a pigment of the color blue?','Which international flag uses only the colors of this web page?','What makes the sun yellow?'];
-// var answer01 = ['Anthocyanins','Carotenoids','Aurite','Chlorophyll'];
-// var answer02 = ['Navy','Royal','Indigo','Klein'];
-// var answer03 = ['Ecuador','Chad','Andorra','Columbia'];
-// var answer04 = ['Distance from the earth.','Temperature','Plasma','Pudding'];
+
 var correctAnswer = 0;
 var wrongAnswer = 0;
-var win = 0;
-var lose = 0;
 var complete = false;
+var currentQuestion = 0;
 var myQuestions = [{
       id:0,
-      question: 'what pigment makes roses red?',
-      answer: 'Anthocyanins',
+      text: 'what pigment makes roses red?',
+      answerNum: 0,
       potentialAnswers: ['Anthocyanins','Carotenoids','Aurite','Chlorophyll']
     },{
       id:1,
-      question: 'Which is a pigment of the color blue?',
-      answer: 'Indigo',
+      text: 'Which is a pigment of the color blue?',
+      answerNum: 2,
       potentialAnswers: ['Navy','Royal','Indigo','Klein']
     },{
       id:2,
-      question: 'Which international flag uses only the colors of this web page?',
-      answer: 'Columbia',
+      text: 'Which international flag uses only the colors of this web page?',
+      answerNum: 3,
       potentialAnswers: ['Ecuador','Chad','Andorra','Columbia']
     },{
       id:3,
-      question: 'What makes the sun yellow?',
-      answer: 'Temperature',
+      text: 'What makes the sun yellow?',
+      answerNum: 1,
       potentialAnswers: ['Distance from the earth.','Temperature','Quantum Foam','Pudding']
   }];
-// var x = myQuestions[0].potentialAnswers.([Math.floor(Math.random() * myQuestions[0].potentialAnswers.length)]);
-// console.log(x);
 
 $(document).ready(function() {
 
-function getIndex() {
-index = 0;
-for(var i=0;i< myQuestions.length;i++){
-  index=myQuestions[i].id;
-  console.log(index);
+  function showAnswers(question) {
+    question.potentialAnswers.forEach(function(answer, i) {
+      var answerDiv = $('<div class="answer">');
+      if(question.answerNum === i){
+        // this is a correct answer
+        answerDiv.click(function() {
+          alert('you\'re answer is correct!');
+          currentQuestion++;
+          correctAnswer++;
+          showQuestion(currentQuestion);
+        });
+      } else {
+        // this is an incorrect answer
+        answerDiv.click(function() {
+          alert('You\'ve selected the wrong answer.');
+          currentQuestion++;
+          wrongAnswer++;
+          showQuestion(currentQuestion);
+        });
+      }
+      // add answer text to div
+      answerDiv.text(answer);
+      $('#answer').append(answerDiv);
+    });
   }
-}
 
-function getQuestion(index) {
-  if(index <= 1) {
-    if (index == 1) {
-      $('#question').text(myQuestions[1].question);
-    }
-    if (index == 2) {
-        $('#question').text(myQuestions[2].question);
-    }
-    if (index == 3) {
-      $('#question').text(myQuestions[3].question);
+  function showQuestion(i) {
+    if(currentQuestion < myQuestions.length) {
+      $('#question').text(myQuestions[i].text);
+      $('#answer').empty();
+      showAnswers(myQuestions[currentQuestion]);
+    } else {
+      console.log('the quiz is over');
+      $('#content01').empty();
+      $('#content02').empty();
+      var resultsCorrect = $('<p>Correct Answers: ' + correctAnswer + '</p>');
+      var resultsWrong = $('<p>Wrong Answers: ' + wrongAnswer + '</p>')
+      $('#jumbotron h1').text('You answered:');
+      $('#jubotron .header').append(resultsCorrect);
     }
   }
-}
 
-  //this is a test
-function question() {
-  $('#question').text(myQuestions[0].question);
-  $('#question').append(myQuestions[1].question);
-  $('#question').append(myQuestions[2].question);
-  $('#question').append(myQuestions[3].question);
-}
-// Get a question from the myQuestions object
-function getAnswer() {
-  //this is a test
-  var answer00 = myQuestions[0].answer;
-  console.log(myQuestions[0].answer);
-  var answer01 = myQuestions[1].answer;
-  console.log(myQuestions[1].answer);
-  var answer02 = (myQuestions[2].answer);
-  console.log(myQuestions[2].answer);
-  var answer03 = myQuestions[3].answer;
-  console.log(myQuestions[3].answer);
-  // pull the answers from the potentialAnswers array and display them in the <p> tags with the class of answer in the display.  currently, the <p> tags are hardcoded in the dom.  It has been suggested, it would be easier to display randoomly selected potentialAnswers and add them to dynamically generated <p> tages ex: ...html('<p class="answer">potentialAnswers[i]</p>).  ???
-}
-function evalutateAnswer() {
-  // check the selected answer to the acual answer presented in the myQuestions object.  If it is correct win++ if not loss ++. evelautate the number of wins and losses if they are >= 5 the game is over
-}
-function gameTimer() {
-  // set a timeOut function for two events.  One create timer that displays and countdowns from 25 in the timer class.  Evalutate if an answer has been selected within 25 seconds.  If an answer is not selected within 25 seconds loss++
-}
-function finish() {
-  // if(questionsAsked<questions.length) {
-    // display correct answers
-    // display incorrect answers
-  // }
-}
-
-$('.start-btn').on('click', function() {
-  //this is a text
-  console.log('I\'ve been clicked');
-  getIndex();
-  question();
-  getAnswer();
-  console.log(index);
-  //the main function of the application.  When start is pressed, the functions of above will be selected. gameTimer(), getQuestions, getAnswer, evalutateAnswer().  Should the order be start game on click then all functions held inside the gameTimer?
-});
-
-
+  $('.start-btn').on('click', function() {
+    // console.log('I\'ve been clicked');
+    // $('.start-btn').addClass('hiden');
+    showQuestion(currentQuestion);
+    $('.start-btn').addClass('hidden');
+  });
 
 });
